fc11bb4566fbd7a7d84adf68b9c3b5110edf4479
Milien <mcder018@live.com>
2022-12-08 16:03:56 -0600

    Cleanup notes and reorder for reading priority

--- srcJava/BetterRepairTIcketsMySQLRepository.java
+++ srcJava/BetterRepairTIcketsMySQLRepository.java
@@ -1,26 +1,5 @@
 import any.necessesary.classes;
 
-/*
-What happens if:
-    - The table name changes?
-    - The database type changes?
-    - The behavior necessary to get a table changes?
-    - The codebase changes database types, or needs to support multiple kinds of database?
-
-From these questions, we can gather:
-    - Dependencies are given, pre-built, to the method that needs them
-    - Easily testable - mocks for the Repos can be passed in from any testing framework that supports Mocking
-    - *quickly* testable - while mocked database frameworks for testing database-dependant code exist,
-        they are extremely slow (~5m for tests) and should only be used for integration tests, as they remove the possibility of test-driven development
-    - Dependency Injection is also a cornerstone of being able to do SOLID correctly. By ONLY implementing changes to conform to Dependency Injection, now:
-        - method depends on an interface, giving us dependency-inversion conformance for free!
-        - method now depends on a _high level_ interface (not MySQL specific). This helps us meet the Interface segregation principle
-            and a separate interface can be created (i.e. "MySQLRepository") can be created as needed so classes don't depend on behavior they don't have to.
-        - method now conforms (better) to the Open-Closed principle. We no longer need to edit it if the behavior of the other repositories change,
-            instead we can simply extend those classes or make a new interface implementation
-
-When all we *actually* care about is setting related fields - we can use instance methods on things passed in to do that
- */
 class BetterRepairTicketsMySQLRepository
 {
     private final RepairTicketRepoInterface repo;
@@ -39,6 +18,23 @@ class BetterRepairTicketsMySQLRepository
         tickets.stream().forEach(ticket -> checkEachForRelationsAndUpdateAsNeeded(ticket, replaceRepo));
         tickets.stream().forEach(ticket -> checkEachForRelationsAndUpdateAsNeeded(ticket, normalRepo));
     }
-
-
 }
+
+/*
+What happens if:
+    - The table name changes?
+    - The database type changes?
+    - The behavior necessary to get a table changes?
+    - The codebase changes database types, or needs to support multiple kinds of database?
+From these questions, we can gather:
+    - Dependencies are given, pre-built, to the method that needs them
+    - Easily testable - mocks for the Repos can be passed in from any testing framework that supports Mocking
+    - *quickly* testable - while mocked database frameworks for testing database-dependant code exist,
+        they are extremely slow (~5m for tests) and should only be used for integration tests, as they remove the possibility of test-driven development
+    - Dependency Injection is also a cornerstone of being able to do SOLID correctly. By ONLY implementing changes to conform to Dependency Injection, now:
+        - method depends on an interface, giving us dependency-inversion conformance for free!
+        - method now depends on a _high level_ interface (not MySQL specific). This helps us meet the Interface segregation principle
+            and a separate interface can be created (i.e. "MySQLRepository") can be created as needed so classes don't depend on behavior they don't have to.
+        - method now conforms (better) to the Open-Closed principle. We no longer need to edit it if the behavior of the other repositories change,
+            instead we can simply extend those classes or make a new interface implementation
+ */
